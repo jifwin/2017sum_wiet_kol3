2bd775ef54f3fa3e7148d8721bc7513f777581c1
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-10 15:46:12 +0200

    add mocked gauss value

--- PlaneTest.py
+++ PlaneTest.py
@@ -1,20 +1,21 @@
 import unittest
-# import unittest.mock as mock
+
+import mock
 
 from Plane import Plane
 
 
 class PlaneTest(unittest.TestCase):
-    def test_should_sth(self):
+    def test_should_apply_correction(self):
         plane = Plane(plane_orientation=15, turbulence_gauss_mean=5, turbulence_gauss_sigma=20)
 
-        def mocked_random_gauss(ignored1, ignored2):
-            return 5
+        mocked_random_gauss = 5
 
-        with mock.patch('random.gauss'):
+        initial_orientation = plane.plane_orientation
+        with mock.patch('random.gauss', lambda x: mocked_random_gauss):
             plane.process()
 
-        self.assertTrue(True)
+        self.assertEquals(plane.plane_orientation, initial_orientation + mocked_random_gauss)
 
     def test_should_count_number_of_steps(self):
         plane = Plane()

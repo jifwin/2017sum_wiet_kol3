50321cbfbcb350fb94e240aa6de450e5c2dc6402
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-16 22:12:48 +0200

    fix test_should_apply_correction test

--- PlaneTest.py
+++ PlaneTest.py
@@ -1,8 +1,6 @@
 import unittest
 
 import mock
-import patch as patch
-
 from Plane import Plane
 
 
@@ -11,20 +9,17 @@ class PlaneTest(unittest.TestCase):
         plane = Plane(plane_orientation=15, turbulence_gauss_mean=5, turbulence_gauss_sigma=20)
 
         mocked_random_gauss = 5
+        mocked_correction = -7
 
         def get_mocked_random_gauss(ignored1, ignored2):
             return mocked_random_gauss
 
-        def get_mocked_correction():
-            return 0
-
         initial_orientation = plane.plane_orientation
         with mock.patch('random.gauss', get_mocked_random_gauss):
-            with patch.object(plane, 'get_correction') as mocked_plane:
-                mocked_plane.get_correction = get_mocked_correction
+            plane.get_correction = lambda x: mocked_correction
             plane.process()
 
-        self.assertEquals(plane.plane_orientation, initial_orientation + mocked_random_gauss)
+        self.assertEquals(plane.plane_orientation, initial_orientation + mocked_random_gauss + mocked_correction)
 
     def test_should_count_number_of_steps(self):
         plane = Plane()
@@ -32,9 +27,10 @@ class PlaneTest(unittest.TestCase):
         for i in xrange(number_of_steps):
             plane.process()
 
-        self.assertEqual(plane.NofStep, number_of_steps+1)
+        self.assertEqual(plane.NofStep, number_of_steps + 1)
+
 
 if __name__ == '__main__':
     unittest.main()
 
-#todo: 12 tests
+# todo: 12 tests

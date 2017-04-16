d2b95a6f616637452a1905e582f0e21e0b23d90d
Grzegorz Pietrusza <gpietrus@akamai.com>
2017-04-16 22:18:51 +0200

    add test_should_use_default_values_not_specified test

--- PlaneTest.py
+++ PlaneTest.py
@@ -5,7 +5,19 @@ from Plane import Plane
 
 
 class PlaneTest(unittest.TestCase):
+    def test_should_use_default_values_not_specified(self):
+        #given
+        plane = Plane()
+
+        #when
+
+        #then
+        self.assertTrue(isinstance(plane.plane_orientation, int))
+        self.assertTrue(isinstance(plane.turbulence_gauss_mean, int))
+        self.assertTrue(isinstance(plane.turbulence_gauss_sigma, int))
+
     def test_should_apply_correction(self):
+        #given
         plane = Plane(plane_orientation=15, turbulence_gauss_mean=5, turbulence_gauss_sigma=20)
 
         mocked_random_gauss = 5
@@ -15,18 +27,25 @@ class PlaneTest(unittest.TestCase):
             return mocked_random_gauss
 
         initial_orientation = plane.plane_orientation
+
+        #when
         with mock.patch('random.gauss', get_mocked_random_gauss):
             plane.get_correction = lambda x: mocked_correction
             plane.process()
 
+        #then
         self.assertEquals(plane.plane_orientation, initial_orientation + mocked_random_gauss + mocked_correction)
 
     def test_should_count_number_of_steps(self):
+        #given
         plane = Plane()
         number_of_steps = 100
+
+        #when
         for i in xrange(number_of_steps):
             plane.process()
 
+        #then
         self.assertEqual(plane.NofStep, number_of_steps + 1)
 
 
